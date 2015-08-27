4e6be6f3e58a8b20fd588033bf16b9918586ad31
Piyush Choudhary <piyush.choudhary.25@gmail.com>
2015-08-27 22:29:43 +0530

    bootstrap integrated

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,15 @@ class User < ActiveRecord::Base
 
   # Relations
   has_many  :glucose_levels
+    
+  # Method to get name of the user.
+  # Login used is as following :
+  # If user have first name or last name, it will be a combination of both.
+  # Else, we will get it from the first part of his email address
+  def get_name
+    name = [first_name, last_name].join(" ")
+    email.split("@").first.gsub(/[^0-9A-Za-z]/, " ") if name.blank?
+    name
+  end
+  
 end
